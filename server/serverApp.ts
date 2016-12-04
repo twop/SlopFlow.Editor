3004164c18ccd0ec67331ab0c2503c6fa817a99c
ObserverAlex <Observer.alex@gmail.com>
2016-12-04 10:52:50 +0300

    #12 removed lite-server, changed package.json

--- server/serverApp.ts
+++ server/serverApp.ts
@@ -1,26 +1,27 @@
 import * as express from 'express';
+import * as path from 'path'
 export class ServerApp {
 
     private _App: express.Express;
-    
     constructor() 
     {
-        this._App = express();        
+        this._App = express(); 
+        this._App.use('/',express.static(path.join(__dirname+'/../')));  
     }
     
     public setRoutes() 
     {        
-        this._App.get('/', this._RenderHelloWorld);          
+      //  this._App.get('/', this.SendIndexHtml);     
     }
 
     public startServer() 
     {
         this._App.listen(3000, function () {
-            console.log('Example app listening on port 5000!');
+            console.log('Server started');
         });
     }
 
-    private _RenderHelloWorld(req: express.Request, res: express.Response) {
-        res.send('Hello World!');
+    private SendIndexHtml(req: express.Request, res: express.Response) {
+      //  res.sendFile('index.html');
     }
 }
